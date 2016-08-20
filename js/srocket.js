235530f7f02a6da098d9b29cf62fadc38a1f435e
Akash Deep <psngh08@gmail.com>
2016-08-20 21:56:40 +0530

    Add files via upload

--- js/srocket.js
+++ js/srocket.js
@@ -1,5 +1,32 @@
 $(document).ready(function(e){
-    $("#start-button").click(function(e){
-        $('.gameBg').css("background-image", "url(../img/home-screen.jpg)");
-    });
-});
+    	$("#start-button").click(function(e){
+	   		$('.gameBg').css("background-image", "url(./img/home-screen.jpg)");
+	   		$('.start-button').css("display", "none");
+	   		$('.home-menu').show();
+	    });
+	    $("#cCampaign").click(function(e){
+	   		$('.gameBg').css("background-image", "url(./img/campaign-bg.jpg)");
+	   		$('.campaign-main').show();
+	   		$('.home-menu').hide();
+	    });
+	    $("#instructions").click(function(e){
+	   		$('.gameBg').css("background-image", "url(./img/main-bg.jpg)");
+	   		$('.inst-main').show();
+	   		$('.home-menu').hide();
+	    });
+	    $("#mPlace").click(function(e){
+	   		$('.gameBg').css("background-image", "url(./img/market-place-bg.jpg)");
+	   		$('.market-main').show();
+	   		$('.home-menu').hide();
+	    });
+	    $("#market-customizebtn").click(function(e){
+	   		$('.gameBg').css("background-image", "url(./img/main-bg.jpg)");
+	   		$('.customize-main').show();
+	   		$('.market-main').hide();
+	    });
+	    $(".planet").click(function(e){
+	   		$('.gameBg').css("background-image", "url(./img/main-bg.jpg)");
+	   		$('.game-play-bg').show();
+	   		$('.campaign-main').hide();
+	    });
+	});
